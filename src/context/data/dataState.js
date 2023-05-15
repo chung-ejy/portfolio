c073c4387eaf821efd2e4f4296ef971837dbde22
chung-ejy <chung.ejy@gmail.com>
2023-05-16 06:54:13 +0900

    added a cookie receiver for production

--- src/context/data/dataState.js
+++ src/context/data/dataState.js
@@ -59,10 +59,25 @@ const DataState = props => {
         });
     }
 
+    const getCookie =(name)=> {
+      let cookieValue = null;
+      if (document.cookie && document.cookie !== '') {
+          const cookies = document.cookie.split(';');
+          for (let i = 0; i < cookies.length; i++) {
+              const cookie = cookies[i].trim();
+              // Does this cookie string begin with the name we want?
+              if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                  cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                  break;
+              }
+          }
+      }
+      return cookieValue;
+  }
     const getPrice = (data) => {
         try {
           setLoading();
-          axios.post(`${base_url}/api/api`, data).then(res => {
+        axios.post(`${base_url}/api/api`, data).then(res => {
                 dispatch({
                 type: GET_PRICE   ,
                 payload: res.data
