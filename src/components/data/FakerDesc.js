0907401b0e74f8a70f742f3b830f928089f02d7b
chung-ejy <chung.ejy@gmail.com>
2023-05-15 10:33:34 +0900

    chatgpt description updates

--- src/components/data/FakerDesc.js
+++ src/components/data/FakerDesc.js
@@ -5,9 +5,10 @@ const FakerDesc = () => {
     const tagline = "Fake News Classifier"
     const dates = "July 2021 - August 2021"
     const projectLink = "https://github.com/chung-ejy/faker"
-    const description = "Description: Fake news classifier based off kaggle data set that can be"
-    const tooltips = "Usage: Type a news article to see if its fake or not lmao "
-    const disclaimer = "Disclaimer: This model has 73% chance to work everytime ..."
+    const description = "Description: Enter the realm of truth and deception with our state-of-the-art Fake News Classifier, meticulously developed using a comprehensive dataset sourced from Kaggle. Experience the power of cutting-edge technology as we unravel the intricate web of misinformation, empowering you to distinguish fact from fiction like never before."
+    const tooltips = "Usage: Harness the potential of our powerful tool by simply typing in a news article. Brace yourself for an enlightening experience as our sophisticated model examines the text, unveiling its verdict on the authenticity of the information provided. With a touch of humor, we invite you to put our classifier to the test and see if it can debunk the falsehoods lurking within the digital landscape."
+    const disclaimer = "Disclaimer: Our Fake News Classifier stands as a testament to our commitment to combating misinformation; however, we provide a candid disclosure. The model boasts an impressive 73% success rate, striving to accurately identify deceptive news articles. Nonetheless, due to the inherent complexity of the subject matter and the ever-evolving nature of misinformation, we cannot guarantee absolute accuracy on every occasion. We encourage critical thinking and further research when encountering news articles. Ultimately, it is important to exercise your own judgment and corroborate information from trusted sources."
+    const dataLink = "https://www.kaggle.com/datasets/clmentbisaillon/fake-and-real-news-dataset?resource=download"
     return(<div className="card cardbody bg-light p-2">
     <h1 className="cardtitle text-primary text-center">{title}</h1>
     <h3 className="text-center">{tagline}</h3>
@@ -22,15 +23,17 @@ const FakerDesc = () => {
                         {projectLink}
                     </a>
                 </td>
+                <td className="d-none d-lg-table-cell">
+                    <a href={`${dataLink}`}>
+                        Link to dataset
+                    </a>
+                </td>
             </tr>
         </tbody>
     </table>
     <FakerForm />
     <p className="m-2">
         {description}
-        <a className="m-1" href="https://www.kaggle.com/datasets/clmentbisaillon/fake-and-real-news-dataset?resource=download">
-            found here.
-        </a> 
     </p>
     <p className="m-2 text-success">
         {tooltips}
--- src/components/data/ShuffleDesc.js
+++ src/components/data/ShuffleDesc.js
@@ -5,9 +5,10 @@ const ShuffleDesc = () => {
     const tagline = "Song Recommender"
     const dates = "July 2021 - August 2021"
     const projectLink = "https://github.com/chung-ejy/shuffle"
-    const description = "Description: Song recommendations based off Spotify playlist dataset that can be found"
-    const tooltips = "Usage: Type a track name alongside the artist. Note: Limited dataset, think 2010s pop .."
-    const disclaimer = "Disclaimer: This project is not an official Spotify product."
+    const description = "Immerse yourself in the realm of music discovery with our extraordinary Song Recommendations tool, powered by a rich dataset sourced from Spotify playlists. Unlock the hidden gems and explore new horizons as we guide you through an unparalleled musical journey based on your preferences and the vast knowledge contained within the dataset."
+    const tooltips = "Embark on an adventure of musical exploration by simply typing the name of a track alongside the artist. Our powerful tool, fueled by a carefully curated dataset, is at your disposal. While the dataset primarily focuses on the vibrant pop music scene of the 2010s, prepare to be pleasantly surprised by the range of genres and artists it encompasses."
+    const disclaimer = "Disclaimer: Please note that this project is not an official Spotify product, but a passion project crafted with love and dedication. We make no claims of official affiliation or endorsement. The recommendations provided are based on the dataset's insights and algorithms, aiming to enhance your music discovery experience. However, individual musical preferences may vary, and we cannot guarantee absolute satisfaction. Sit back, relax, and let the melodies guide you, knowing that any decisions made regarding music consumption are entirely at your own discretion."
+    const dataLink = "https://www.aicrowd.com/challenges/spotify-million-playlist-dataset-challenge/dataset_files"
     return(<div className="card cardbody bg-light p-2">
     <h1 className="cardtitle text-primary text-center">{title}</h1>
     <h3 className="text-center">{tagline}</h3>
@@ -22,15 +23,17 @@ const ShuffleDesc = () => {
                         {projectLink}
                     </a>
                 </td>
+                <td className="d-none d-lg-table-cell">
+                    <a href={`${dataLink}`}>
+                        Link to dataset
+                    </a>
+                </td>
             </tr>
         </tbody>
     </table>
     <ShuffleForm />
     <p className="m-2">
         {description}
-        <a className="m-1" href="https://www.aicrowd.com/challenges/spotify-million-playlist-dataset-challenge/dataset_files">
-            found here.
-        </a> 
     </p>
     <p className="m-2 text-success">
         {tooltips}
