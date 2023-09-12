97658a8ac1eb74551afcd3e004e6d7db8198d4ec
eric <chung.ejy@gmail.com>
2023-09-11 21:28:01 -0400

    got rid of weight

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import DataState from "./context/data/dataState"
 import Header from "./components/layout/Header"
 import Profile from "./components/pages/Profile"
-import Photos from './components/pages/Photos'
 import Projects from './components/pages/Projects'
-// import Blog from './components/pages/Blog.js'
 import background from './assets/bg2.png'
 import FullStackServices from './components/pages/FullStackServices'
 export const App = () => {
@@ -16,7 +14,6 @@ export const App = () => {
                 <Header />
                     <Routes>
                         <Route exact path ="/" element={<Profile />} />
-                        <Route exact path ="/photos" element={<Photos />} />
                         <Route exact path ="/projects" element={<Projects />} />
                         <Route exact path ="/services" element={<FullStackServices />} />
                     </Routes>
